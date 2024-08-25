17baf81f4eabae21b1cdad28dad9d68bc3b3019e
Kevin Anchukaitis <kevin.anchukaitis@gmail.com>
2024-08-25 10:00:41 -0700

    update people page with current positions and graduates

--- people_files/stacks_page_page8.js
+++ people_files/stacks_page_page8.js
@@ -23,40 +23,6 @@ stacks.stacks_in_143 = {};
 stacks.stacks_in_143 = (function(stack) {
 var jQuery = stacks.jQuery;var $ = jQuery;// Start Rinse stack each Javascript code$(document).ready(function(){	if (document.createElement("detect").style.webkitFilter != "") {		// non webkit browser to use canvas method.		if($("#stacks_in_143 img").parent().is("a")){			$("#stacks_in_143 img").parent().addClass("dooRinse");		}else{			$("#stacks_in_143 img").wrap("<span class='dooRinse'></span>");		}				$('#stacks_in_143 .dooRinse').RinseImage({		        hoverEffect : true,		        webworkerPath : "people_files/rinseAssets/",		        invertHoverEffect: true,		        // this option works only on the modern browsers ( on IE lower than 9 it remains always 1)		        intensity:1,		        speed: { 		            fadeIn: 300,		            fadeOut: 300		        }		});	}       });// End Rinse stack each Javascript code
 return stack;})(stacks.stacks_in_143);
-stacks.stacks_in_149 = {};
-stacks.stacks_in_149 = (function(stack) {
-var jQuery = stacks.jQuery;var $ = jQuery;$(function(){
-	 
-	$("#stacks_in_149 img").one("load", function() {  
-		$(this).addClass('show'); 
-	}).each(function() { 
-		if(this.complete) $(this).load(); 
-	}); 
-	
-});
-
-return stack;})(stacks.stacks_in_149);
-stacks.stacks_in_161 = {};
-stacks.stacks_in_161 = (function(stack) {
-var jQuery = stacks.jQuery;var $ = jQuery;// Start Rinse stack each Javascript code$(document).ready(function(){	if (document.createElement("detect").style.webkitFilter != "") {		// non webkit browser to use canvas method.		if($("#stacks_in_161 img").parent().is("a")){			$("#stacks_in_161 img").parent().addClass("dooRinse");		}else{			$("#stacks_in_161 img").wrap("<span class='dooRinse'></span>");		}				$('#stacks_in_161 .dooRinse').RinseImage({		        hoverEffect : true,		        webworkerPath : "people_files/rinseAssets/",		        invertHoverEffect: true,		        // this option works only on the modern browsers ( on IE lower than 9 it remains always 1)		        intensity:1,		        speed: { 		            fadeIn: 300,		            fadeOut: 300		        }		});	}       });// End Rinse stack each Javascript code
-return stack;})(stacks.stacks_in_161);
-stacks.stacks_in_127 = {};
-stacks.stacks_in_127 = (function(stack) {
-var jQuery = stacks.jQuery;var $ = jQuery;$(function(){
-	 
-	$("#stacks_in_127 img").one("load", function() {  
-		$(this).addClass('show'); 
-	}).each(function() { 
-		if(this.complete) $(this).load(); 
-	}); 
-	
-});
-
-return stack;})(stacks.stacks_in_127);
-stacks.stacks_in_159 = {};
-stacks.stacks_in_159 = (function(stack) {
-var jQuery = stacks.jQuery;var $ = jQuery;// Start Rinse stack each Javascript code$(document).ready(function(){	if (document.createElement("detect").style.webkitFilter != "") {		// non webkit browser to use canvas method.		if($("#stacks_in_159 img").parent().is("a")){			$("#stacks_in_159 img").parent().addClass("dooRinse");		}else{			$("#stacks_in_159 img").wrap("<span class='dooRinse'></span>");		}				$('#stacks_in_159 .dooRinse').RinseImage({		        hoverEffect : true,		        webworkerPath : "people_files/rinseAssets/",		        invertHoverEffect: true,		        // this option works only on the modern browsers ( on IE lower than 9 it remains always 1)		        intensity:1,		        speed: { 		            fadeIn: 300,		            fadeOut: 300		        }		});	}       });// End Rinse stack each Javascript code
-return stack;})(stacks.stacks_in_159);
 stacks.stacks_in_258 = {};
 stacks.stacks_in_258 = (function(stack) {
 var jQuery = stacks.jQuery;var $ = jQuery;$(function(){
